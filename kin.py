52c42e335eeb9ee3b8d71149b3f0f3268377129a
VAN TOAN VO <vantoan591@gmail.com>
2022-11-30 21:37:08 -0600

    kinematic analysis done

--- kin.py
+++ kin.py
@@ -1,33 +1,109 @@
 from sympy import *
+from numpy import linspace
+import matplotlib.pyplot as  plt
+from sympy import lambdify
+import math as m
 
-## Write result to an output file
-## Python calculation based on diagram
 
-#time
+# time variable //GLOBAL VARIABLES
 t = symbols('t')
 # Angular velocity of crank
-omega = symbols('\omega')
+omega = symbols('omega')
 # radius of crank
 r = symbols('r')
 # length of conrod
 l = symbols('l')
 
-# Angular position of crank
-theta = omega * t
+def calculation_formula(t, omega, r, l):
+    # Angular position of crank
+    theta = omega * t
+    
+    # sin(phi)
+    sPhi = r / l * sin(omega * t)
 
-# sin(phi)
-sPhi = r / l * sin(omega * t)
+    # cos(phi)
+    cPhi = sqrt(1 - sPhi ** 2)
 
-# cos(phi)
-cPhi = sqrt(1 - sPhi ** 2)
+    # Position of the slider (piston)
+    x = r * cos(theta) + l * cPhi
+    
+    # Velocity of the slider
+    xdot = diff(x, t)
+    
+    # acceleration
+    xddot = diff(xdot, t)
+    
+    print("------------------------")
+    print("The mathematical formula of the kinematic analysis in LATEX form: \n")
+    print(f"Formula of the equation of motion in LATEX form: {latex(x)}")
+    print(f"Formula of the equation of velocity in LATEX form: {latex(xdot)}")
+    print(f"Formula of the equation of acceleration in LATEX form: {latex(xddot)}")
+    print("------------------------\n")
+    
+    return [x, xdot, xddot]
 
-# Position of the slider (piston)
-x = r * cos(theta) + l * cPhi
+## This function generate the kinematic analysis base on the variable (engine properties) passed in
+def kinematics():
+    print("\nTo perform the kinematic analysis, please input your engine properties at the exact motor speed (The motor speed increases when accelerating and decreases when decelerate): \n")
+    rV = float(input("Please enter the radius of the crank (in): "))
+    lV = float(input("Please enter length of conrod (in): "))
+    omegaRpm = float(input("Please enter the angular velocity of the engine (rpm): "))   
+    omegaRad = omegaRpm * 2 * m.pi / 60
+    vars = {r: rV, l: lV, omega: omegaRad}
+    
+    result = calculation_formula(t, omega, r, l)
+    # Position
+    x = result[0]
+    x = x.subs(vars)
+    
+    lam_x = lambdify(t, x, modules=['numpy'])
+    
+    # Velocity
+    xdot = result[1]
+    xdot = xdot.subs(vars)
+    lam_xdot = lambdify(t, xdot, modules=['numpy'])
+    
+    # acceleration
+    xddot = result[2]
+    xddot = xddot.subs(vars)
+    lam_xddot = lambdify(t, xddot, modules=['numpy'])
+    
+    ## Generating kinamatics analysis plot
+    ## We plot the x, xdot, and xddot in 2 time period of rotation in second:
+    timePeriod = 60 / (omegaRpm)
+    t_vals = linspace(0, timePeriod * 2, 720)
+    
+    x_vals = lam_x(t_vals)
+    xdot_vals = lam_xdot(t_vals)
+    xddot_vals = lam_xddot(t_vals)
+    
+    #plotting
 
-# Velocity of the slider
-xdot = diff(x, t)
-#print(latex(xdot))
+    figure, axis = plt.subplots(3, figsize = (10, 6))
+    figure.suptitle("Kinematic Analysis of Engine")
+    
+    plt.tight_layout()
+    engineInfo = f"Engine Infomation: \nRadius of crank: {rV} in\nLength of conrol: {lV} in\nomega: {omegaRpm} rpm"
+    plt.gcf().text(0.01, 0.5, engineInfo, fontsize = 10)
+    plt.subplots_adjust(left = 0.25)
+    
+    axis[0].plot(t_vals, x_vals)
+    axis[0].set_title("Position Analysis")
+    axis[0].set(xlabel = "Time (s)", ylabel = "Position (in)")
+    
+    axis[1].plot(t_vals, xdot_vals)
+    axis[1].set_title("Velocity Analysis")
+    axis[1].set(xlabel = "Time (s)", ylabel = "Velocity (in/s)")
+    
+    axis[2].plot(t_vals, xddot_vals)
+    axis[2].set_title("Acceleration Analysis")
+    axis[2].set(xlabel = "Time (s)", ylabel = "Acceleration (in/s^2)")
+    
+    plt.show()
 
-# acceleration
-xddot = diff(xdot, t)
-#print(latex(xddot))
+def main():
+    calculation_formula(t, omega, r, l)
+    kinematics()
+    
+if __name__ == "__main__":
+    main()
