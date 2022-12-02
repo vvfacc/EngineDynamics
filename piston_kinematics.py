a325ef00e7843cbdb60d96d6f376564118cf6c57
bvp125 <53663446+bvp125@users.noreply.github.com>
2022-12-01 19:41:35 -0600

    Added comments for readability

--- piston_kinematics.py
+++ piston_kinematics.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as  plt
 from sympy import lambdify
 import math as m
 
-
 # time variable //GLOBAL VARIABLES
 t = symbols('t')
 # Angular velocity of crank
 omega = symbols('omega')
-# radius of crank
+# length of crank
 r = symbols('r')
-# length of conrod
+# length of connecting rod
 l = symbols('l')
 
 def calculation_formula(t, omega, r, l):
@@ -24,34 +23,35 @@ def calculation_formula(t, omega, r, l):
     # cos(phi)
     cPhi = sqrt(1 - sPhi ** 2)
 
-    # Position of the slider (piston)
+    # Position of the piston
     x = r * cos(theta) + l * cPhi
     
-    # Velocity of the slider
+    # Velocity of the piston
     xdot = diff(x, t)
     
-    # acceleration
+    # Acceleration of the piston
     xddot = diff(xdot, t)
     
     print("------------------------")
     print("The mathematical formula of the kinematic analysis in LATEX form: \n")
-    print(f"Formula of the equation of motion in LATEX form: {latex(x)}")
+    print(f"Formula of the equation of position in LATEX form: {latex(x)}")
     print(f"Formula of the equation of velocity in LATEX form: {latex(xdot)}")
     print(f"Formula of the equation of acceleration in LATEX form: {latex(xddot)}")
     print("------------------------\n")
     
     return [x, xdot, xddot]
 
-## This function generate the kinematic analysis base on the variable (engine properties) passed in
+## This function generates the kinematic analysis based on inputs related to the engine properties
 def kinematics():
-    print("\nTo perform the kinematic analysis, please input your engine properties at the exact motor speed (The motor speed increases when accelerating and decreases when decelerate): \n")
-    rV = float(input("Please enter the radius of the crank (in): "))
-    lV = float(input("Please enter length of conrod (in): "))
+    print("\nTo perform the kinematic analysis, please input your engine properties at the exact motor speed (The motor speed increases when accelerating and decreases when decelerating): \n")
+    rV = float(input("Please enter the length of the crank (in): "))
+    lV = float(input("Please enter length of connecting rod (in): "))
     omegaRpm = float(input("Please enter the angular velocity of the engine (rpm): "))   
     omegaRad = omegaRpm * 2 * m.pi / 60
     vars = {r: rV, l: lV, omega: omegaRad}
     
     result = calculation_formula(t, omega, r, l)
+    
     # Position
     x = result[0]
     x = x.subs(vars)
@@ -63,13 +63,13 @@ def kinematics():
     xdot = xdot.subs(vars)
     lam_xdot = lambdify(t, xdot, modules=['numpy'])
     
-    # acceleration
+    # Acceleration
     xddot = result[2]
     xddot = xddot.subs(vars)
     lam_xddot = lambdify(t, xddot, modules=['numpy'])
     
     ## Generating kinamatics analysis plot
-    ## We plot the x, xdot, and xddot in 2 time period of rotation in second:
+    ## We plot the x, xdot, and xddot in 2 time periods of rotation in second:
     timePeriod = 60 / (omegaRpm)
     t_vals = linspace(0, timePeriod * 2, 720)
     
@@ -77,13 +77,12 @@ def kinematics():
     xdot_vals = lam_xdot(t_vals)
     xddot_vals = lam_xddot(t_vals)
     
-    #plotting
-
+    # Plotting
     figure, axis = plt.subplots(3, figsize = (10, 6))
     figure.suptitle("Kinematic Analysis of Engine")
     
     plt.tight_layout()
-    engineInfo = f"Engine Infomation: \nRadius of crank: {rV} in\nLength of conrol: {lV} in\nomega: {omegaRpm} rpm"
+    engineInfo = f"Engine Infomation: \nCrank length: {rV} in\nConrod length: {lV} in\nEngine speed: {omegaRpm} rpm"
     plt.gcf().text(0.01, 0.5, engineInfo, fontsize = 10)
     plt.subplots_adjust(left = 0.25)
     
